4533db9f8dfd604012642380c438abedcf9cbdfa
Terencekgithub <145474831+Terencekgithub@users.noreply.github.com>
2023-10-16 23:25:22 +0200

    Update nginx.sh

--- labs/nginx.sh
+++ labs/nginx.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
-$sudo apt-get update
-$sudo apt-get -y install nginx
-$systemctl is-active nginx
-$sudo systemctl start nginx
-$systemctl is-active nginx
-$sudo mkdir /var/www/html/labs
-$ls -ld /var/www/html/labs
-$sudo chown terencenhlanhla:root /var/www/html/labs
-$ln -s /var/www/html/labs html
-$ls -l
-$cd html
-$touch index.html
-$nano index.html
+echo "TERENCE KAMOGELO MOABI 221716971"
+echo "NHLANHLA MAKOFANE"
+
+echo "CHECK IF NGINX IS INSTALLED ON THE WEB SERVER"
+systemctl is-active nginx
+
+echo "IF NGINX IS INSTALLED, CHECK IF NGINX IS RUNNING IF NOT THEN START IT"
+apt-get update
+apt-get -y install nginx
+systemctl start nginx
+
+echo "CREATE A DIRECTORY CALLED LABS"
+mkdir /var/www/html/labs
+ls -ld /var/www/html/labs
+
+echo "CHANGE OWNERSHIP OF DIRECTORY TO CURRENT USER"
+chown terencenhlanhla:root /var/www/html/labs
+
+echo "CREATE A SYMBOLIC LINK CALLED HTML UNDER HOME DIRECTORY"
+ln -s /var/www/html/labs html
+ls -l
+
+echo "CREATE AN INDEX.html FILE"
+cd html
+touch index.html
+
+echo "ADDING INFORMATION TO THE INEDX.HTML FILE"
+nano index.html
+<!DOCTYPE html>
+<html>
+<body>
+<h1>STUDENT INFORMATION PAGE</h1>
+<p>INITIALS: TKM AND NVM
+   NAME: TERENCE MOABI AND NHLANHLA MAKOFANE
+   STUDENT NO: 221716971 AND 221920234
+   COURSE: COMPUTER SYSTEMS
+</p>
+</body>
+</html>
+
+

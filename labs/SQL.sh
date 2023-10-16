f1f75afab1e90f1a05fbe6d6898e6eb51a75ee46
Terencekgithub <145474831+Terencekgithub@users.noreply.github.com>
2023-10-16 23:30:52 +0200

    Create SQL.sh

---
+++ labs/SQL.sh
@@ -0,0 +1,22 @@
+echo "Access MySQL using the root user"
+mysql -u root -p
+
+echo "Create the student_records database"
+CREATE DATABASE student_records;
+
+echo "Use the student_records database"
+USE student_records;
+
+echo "Create the students table"
+CREATE TABLE students (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    course VARCHAR(255) NOT NULL
+);
+
+echo "Insert sample data into the students table"
+INSERT INTO students (name, age, gender, course) VALUES
+('Terence Moabi', 21, 'Male', 'Computer Systems'),
+('Nhlanhla Makofane', 22, 'Male', 'Computer Systems');
